fad245d480357eba1926562fb6d9b560ed6b9de8
myavidziqbal <sademark08@gmail.com>
2018-11-12 18:31:08 +0700

    Initial Commit

---
+++ api/products/controller.js
@@ -0,0 +1,32 @@
+const models = require("../models");
+
+exports.getAll = (req, res) => {
+  models.products
+    .findAll()
+    .then(products => res.send(products))
+    .catch(err => console.log(err));
+};
+
+exports.post = (req, res) => {
+  models.products
+    .create(req.body)
+    .then(product => res.send(product))
+    .catch(err => res.send(err));
+};
+
+exports.deleteOne = (req, res) => {
+  models.products
+    .findOne({ where: { id: req.params.id } })
+    .then(products => products.destroy())
+    .then(result => res.send(result))
+    .catch(err => res.send(err));
+};
+
+exports.search = (req, res) => {
+  models.products
+    .findAll({
+      where: req.query
+    })
+    .then(products => res.send(products))
+    .catch(err => res.send(err));
+};
---
+++ api/products/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("./controller");
+
+router.get("/:id", controller.search);
+router.get("/", controller.getAll);
+router.post("/", controller.post);
+router.delete("/:id", controller.deleteOne)
+
+module.exports = router;
